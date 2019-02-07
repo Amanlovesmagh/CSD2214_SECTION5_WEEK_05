d15a13bc0c351f72ca4f2002f0db29627a0f13da
visualstudio <visualstudio@230-PC31>
2019-02-07 12:02:02 -0500

    commit

--- CSD2214_Week_5/js/main.js
+++ CSD2214_Week_5/js/main.js
@@ -2,8 +2,23 @@
 function myFunction(a, b){
     return a+b;
 }
-function arrays{
+function arrays(){
     var pens;
-    pns = ["red", orange",green", "blue", "purple"];
+    pens = ["red", "orange", "green", "blue", "purple"];
     console.log(pens);
+    console.log(pens[0]);
+    for (i = 0; i < pens.length; i++) {
+        alert(pens[i]);
+    }
+    alert("Length of array is :" + pens.length);
+    /*
+    pens = new Array("red", "orange", "green", "blue", "purple");
+    markers = pens;
+    console.log(markers);
+    console.log(markers[0]);
+    var i;
+    for (i = 0; i < markers.length; i++) {
+        alert(markers[i]);
+    }
+    alert("Length of array is :", + markers.length);*/
 }
